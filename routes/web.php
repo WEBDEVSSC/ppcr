d456f3a1d58a37880295c283acaac6d5f00d4fe5
WEB DEV SSC <soportewebssc@gmail.com>
2025-02-16 23:42:34 -0600

    RUTA PRINCIPAL

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,14 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+Route::get('/', function () {
+    return redirect()->route('login');
 });
 
 Auth::routes();
