b80fa6cff22ccc66f87cd71dd48eca0e2d124591
WEB DEV SSC <soportewebssc@gmail.com>
2025-02-19 11:33:06 -0600

    CONFIG DB

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
